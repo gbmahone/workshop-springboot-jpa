132fc27884957882c61b1819367191af6df27468
Gabriela Peixoto <gabrielapeixoto@example.com>
2026-04-15 17:28:24 +0100

    OrderItem, many-to-many association with extra attributes

--- src/main/java/com/udemy/course/config/TestConfig.java
+++ src/main/java/com/udemy/course/config/TestConfig.java
@@ -1,14 +1,8 @@
 package com.udemy.course.config;
 
-import com.udemy.course.entities.Category;
-import com.udemy.course.entities.Order;
-import com.udemy.course.entities.Product;
-import com.udemy.course.entities.User;
+import com.udemy.course.entities.*;
 import com.udemy.course.entities.enums.OrderStatus;
-import com.udemy.course.repositories.CategoryRepository;
-import com.udemy.course.repositories.OrderRepository;
-import com.udemy.course.repositories.ProductRepository;
-import com.udemy.course.repositories.UserRepository;
+import com.udemy.course.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
@@ -33,6 +27,9 @@ public class TestConfig implements CommandLineRunner {
 
     @Autowired
     private ProductRepository productRepository;
+
+    @Autowired
+    private OrderItemRepository orderItemRepository;
 
     @Override
     public void run(String... args) throws Exception {
@@ -70,6 +67,13 @@ public class TestConfig implements CommandLineRunner {
 
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+
+        OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+        OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+        OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+        OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+
+        orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 
     }
 
--- src/main/java/com/udemy/course/entities/OrderItem.java
+++ src/main/java/com/udemy/course/entities/OrderItem.java
@@ -1,5 +1,6 @@
 package com.udemy.course.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.udemy.course.entities.pk.OrderItemPK;
 import jakarta.persistence.Embeddable;
 import jakarta.persistence.EmbeddedId;
@@ -17,7 +18,7 @@ public class OrderItem implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @EmbeddedId
-    private OrderItemPK id;
+    private OrderItemPK id = new OrderItemPK();
     private Integer quantity;
     private Double price;
 
@@ -33,6 +34,7 @@ public class OrderItem implements Serializable {
 
     }
 
+    @JsonIgnore
     public Order getOrder() {
         return id.getOrder();
     }
