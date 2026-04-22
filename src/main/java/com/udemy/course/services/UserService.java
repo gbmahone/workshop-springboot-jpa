511c3c81803884a5df95908ad638e35e8f422bc6
Gabriela Peixoto <gabrielapeixoto@example.com>
2026-04-22 18:04:45 +0100

    Exception handling - delete

--- src/main/java/com/udemy/course/services/UserService.java
+++ src/main/java/com/udemy/course/services/UserService.java
@@ -2,8 +2,11 @@ package com.udemy.course.services;
 
 import com.udemy.course.entities.User;
 import com.udemy.course.repositories.UserRepository;
+import com.udemy.course.services.exceptions.DatabaseException;
 import com.udemy.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -29,7 +32,14 @@ public class UserService {
     }
 
     public void delete(Long id){
-        repository.deleteById(id);
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e){
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e){
+            throw new DatabaseException(e.getMessage());
+        }
+
     }
 
     public User update(Long id, User obj) {
